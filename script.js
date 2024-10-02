748eab418ae97fac1e9e842944515cc503d1633c
OgliariNatan <natanogliari@gmail.com>
2024-10-02 11:15:00 -0300

    quase lá

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+carregaTarefas();
+
 
 // Adicionar tarefa
 $('#addTarefaBtn').on('click', function () {
@@ -7,8 +9,11 @@ $('#addTarefaBtn').on('click', function () {
     //console.log(tarefaText) //mostra na tela
 
     if (tarefaText.length > 0 ){ //Se tiver tarefa ele adiciona
-        addTarefas(tarefaText) //adiciona tarefa
+        addTarefas(tarefaText); //adiciona tarefa
+        salvarTarefas();
+        $('#tarefaInput').val('');
     } 
+    
   });
 
   //Função para adicionar tarefas
@@ -20,8 +25,33 @@ $('#addTarefaBtn').on('click', function () {
 
   //mARCA como concluido
   $(document).on('click', 'li', function(){
-    $(this).toggleClass('completed');
-
+    $(this).toggleClass('completed');//adiciona a classe completed no li
+    salvarTarefas();
   });
+
+  //remove tarefa
+  $(document).on('click', 'span', function(e){
+    e.stopPropagation();//não deixa aplicar na pai
+    $(this).parent().fadeOut(200, function() {
+        $(this).remove();
+        salvarTarefas();
+    });
+  });
+
+
+  //Função para salvar tarefas
+  function salvarTarefas() {
+    var tarefa = $('tarefaList').html();
+    localStorage.setItem('tarefas', tarefa);
+    
+  }
+  //Função para carregar as tarefas
+  function carregaTarefas() {
+    var tarefas = localStorage.getItem('tarefas');
+    //verifica se exitem dados 
+    if (tarefas) {
+        $('#tarefaList').html(tarefas);
+    }
+  }
 
 })
